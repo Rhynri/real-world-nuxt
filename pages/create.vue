471d3d0c091c473d894dd864bb76658007402d47
Rhynri <rhynri@me.com>
2020-05-13 12:30:26 -0500

    SEO with Vue-Meta

--- pages/create.vue
+++ pages/create.vue
@@ -6,7 +6,18 @@
 
 <script>
 export default {
-  name: 'Create'
+  head() {
+    return {
+      title: 'Create an Event',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'You can create a new event in your neighborhood'
+        }
+      ]
+    };
+  }
 };
 </script>
 
